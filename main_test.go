884ca6efc9824d42f55d5bbbd0b1112e7df58e5b
Omer <silentminds1234@hotmail.com>
2019-03-19 03:25:14 +0800

    Added comments + test cases

--- main_test.go
+++ main_test.go
@@ -1,8 +1,9 @@
 package main
-
+//SOME ADDED TEST CASES . Also please note that the semver library being used is the one used in main.go as well for
+//obvious compatibility
 import (
 	"testing"
-  "fmt"
+	"fmt"
 	"github.com/Masterminds/semver"
 )
 func myNewVersion (version string) *semver.Version {
@@ -56,25 +57,25 @@ func TestLatestVersions(t *testing.T) {
 			minVersion:     myNewVersion("2.2.1"),
 		},
 		// Implement more relevant test cases here, if you can think of any
-    //All versions lower than min Version so return empty slice
+    		//All versions lower than min Version so return empty slice
     {
 			versionSlice:   []string{"1.10.1", "1.9.5", "1.8.10", "1.10.0", "1.7.14", "1.8.9", "1.9.5"},
 			expectedResult: []string{},
 			minVersion:     myNewVersion("1.11.0"),
 		},
-    // version slice empty
+    		// version slice empty
     {
 			versionSlice:   []string{},
 			expectedResult: []string{},
 			minVersion:     myNewVersion("1.2.0"),
 		},
-    //Prerelease vs No pre release
+    		//Prerelease vs No pre release
     {
 			versionSlice:   []string{"1.8.11", "1.9.6", "1.9.5", "1.8.10","1.11.0-beta","1.11.0", "1.10.1-beta", "1.7.14", "1.8.9", "1.9.5"},
 			expectedResult: []string{"1.11.0","1.10.1-beta", "1.9.6",},
 			minVersion:     myNewVersion("1.9.0"),
 		},
-    //Major version number difference
+    		//Major version number difference,not only minor number difference
     {
 			versionSlice:   []string{"1.8.11", "1.9.6", "1.9.5", "1.8.10","1.11.0-beta","2.0.0","2.0.1","2.1.0","2.1.0-beta","1.11.0", "1.10.1-beta", "1.7.14", "1.8.9", "1.9.5"},
 			expectedResult: []string{"2.1.0","2.0.1","1.11.0","1.10.1-beta", "1.9.6",},
@@ -84,10 +85,10 @@ func TestLatestVersions(t *testing.T) {
 
 	test := func(versionData []string, expectedResult []string, minVersion *semver.Version) {
 		stringSlice := versionToStringSlice(LatestVersions(stringToVersionSlice(versionData), minVersion))
-    if (len(stringSlice)!=len(expectedResult)){
-      t.Errorf("Received %s ,expected %s",stringSlice,expectedResult)
-      return
-    }
+		if (len(stringSlice)!=len(expectedResult)){
+			t.Errorf("Received %s ,expected %s",stringSlice,expectedResult)
+			return
+		}
 		for i, versionString := range stringSlice {
 			if versionString != expectedResult[i] {
 				t.Errorf("Received %s, expected %s", stringSlice, expectedResult)
